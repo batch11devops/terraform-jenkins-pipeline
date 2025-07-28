2e18e759d7e97ca7c55972b058b34abe59904787
batch11devops <rakshashetty722@gmail.com>
2025-07-28 22:05:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,18 @@
-terraform {
-  required_providers {
-    local = {
-      source = "hashicorp/local"
-      version = "~> 2.0"
-    }
-  }
-}
 provider "local" {}
-resource "local_file" "file1" {
-  content  = "This is file 1"
-  filename = "${path.module}/output/file1.txt"
+resource "local_file" "dir1_placeholder" { 
+filename = "${path.module}/dir1/.placeholder" 
+content = "Directory 1 created"
+}
+resource "local_file" "dir2_placeholder" { 
+filename = "${path.module}/dir2/.placeholder" 
+content = "Directory 2 created"
+}
+resource "local_file" "file1" { 
+filename = "${path.module}/file1.txt" 
+content = "This is file 1"
 }
 resource "local_file" "file2" {
-  content  = "This is file 2"
-  filename = "${path.module}/output/file2.txt"
-}
-resource "null_resource" "dir1" {
-  provisioner "local-exec" {
-    command = "mkdir -p ${path.module}/output/dir1"
-  }
-}
-resource "null_resource" "dir2" {
-  provisioner "local-exec" {
-    command = "mkdir -p ${path.module}/output/dir2"
-  }
+ filename = "${path.module}/file2.txt" 
+content = "This is file 2"
 }
 
